0aee55e88f8e6f6228579f62f299b7b6efad57e8
Stefan Mocek <stefan.mocek1@gmail.com>
2022-05-28 14:53:21 +0200

    env 1.0

--- myApp.js
+++ myApp.js
@@ -2,17 +2,12 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-if (process.env.MESSAGE_STYLE==uppercase){
-    app.get('/json', function(req, res) {
-      res.json({ "message": "HELLO JSON" })
-    }); 
-  }
-  else {
-    app.get('/json', function(req, res) {
-      res.json({ "message": "Hello json" })
-  }); 
-  
-  }
+app.get("/json", (req,res)=>{
+    if (process.env.MESSAGE_STYLE===uppercase){
+        return res.json({"message": "HELLO JSON"})
+    }
+        res.json({"messege": "Hello json"})
+  })
 
 
 
