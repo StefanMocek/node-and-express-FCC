8bad82a39848f3c56a897b312cf86484ea812800
Stefan Mocek <stefan.mocek1@gmail.com>
2022-05-28 15:26:14 +0200

    env 1.3

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
-require('dotenv').config();
+const process = require('dotenv').config();
+
+app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/json", (req,res)=>{
     if (process.env.MESSAGE_STYLE=="uppercase"){
